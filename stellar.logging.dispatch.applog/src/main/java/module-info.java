250e5648f66229eea8b220130efaf7d48beadceb
Mechite <contact@mechite.com>
2025-11-28 04:48:32 +0000

    Create `stellar.logging.dispatch.applog` (read details)
    - Create an avaje applog finder for the existing JSR379 System.Logger
      implementation
    
      - This removes one level of indirection for the libraries that may be
        using this method of finding `System.Logger`
    
      - This depends on the existing implementation, it does not duplicate
    
    - Fix the `stellar.bom` that was incorrectly referencing `-dispatch-`
      and `-collect` artifact IDs
    
    Signed-off-by: Mahied Maruf <contact@mechite.com>

---
+++ stellar.logging.dispatch.applog/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+module sirius.stellar.logging.applog {
+	requires org.jspecify;
+	requires sirius.stellar.facility;
+	requires sirius.stellar.logging;
+
+	requires sirius.stellar.logging.jsr379x;
+	requires io.avaje.applog;
+
+	exports sirius.stellar.logging.dispatch.applog;
+
+	provides io.avaje.applog.AppLog.Provider
+			with sirius.stellar.logging.dispatch.applog.AppLogProvider;
+}
---
+++ stellar.logging.dispatch.applog/src/main/java/sirius/stellar/logging/dispatch/applog/AppLogProvider.java
@@ -0,0 +1,26 @@
+package sirius.stellar.logging.dispatch.applog;
+
+import sirius.stellar.logging.dispatch.jsr379x.Jsr379Dispatcher;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+/// Implementation of [io.avaje.applog.AppLog.Provider] used for obtaining instances of [Jsr379Dispatcher].
+///
+/// @author Mahied Maruf (mechite)
+/// @since 1.0
+public final class AppLogProvider implements io.avaje.applog.AppLog.Provider {
+
+	private static final Map<String, Jsr379Dispatcher> loggers = new HashMap<>();
+
+	@Override
+	public System.Logger getLogger(String name) {
+		return loggers.computeIfAbsent(name, Jsr379Dispatcher::new);
+	}
+
+	@Override
+	public System.Logger getLogger(String name, ResourceBundle bundle) {
+		return loggers.computeIfAbsent(name, Jsr379Dispatcher::new);
+	}
+}
