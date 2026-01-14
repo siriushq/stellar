1abffdd58f799ae92fd3c27aabb48fa105a0ec6e
Mechite <contact@mechite.com>
2026-01-14 17:02:50 +0000

    Optimize MDC/NDC in `stellar.logging.dispatch.jboss`
    - Use `ThreadLocal` instead of concurrent collections, matching the
      proper semantics for MDC/NDC, and avoiding locking
    
    - Use `ThreadLocal#remove` instead of clearing collections, in
      `#clearMdc` and `#clearNdc`, also allowing all memory to be
      completely released this way, after usage of this API
    
    - Formatting changes in `#pushNdc`
    
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- stellar.logging.dispatch.jboss/src/main/java/sirius/stellar/logging/dispatch/jboss/JbossProvider.java
+++ stellar.logging.dispatch.jboss/src/main/java/sirius/stellar/logging/dispatch/jboss/JbossProvider.java
@@ -1,9 +1,11 @@
 package sirius.stellar.logging.dispatch.jboss;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
+
+import static java.lang.ThreadLocal.withInitial;
 
 /// Implementation of [org.jboss.logging.LoggerProvider] used for obtaining instances of [JbossDispatcher].
 ///
@@ -11,8 +13,8 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 /// @since 1.0
 public final class JbossProvider implements org.jboss.logging.LoggerProvider {
 
-	private final Map<String, Object> mdc = new ConcurrentHashMap<>();
-	private final Deque<JbossNdcEntry> ndc = new ConcurrentLinkedDeque<>();
+	private final ThreadLocal<Map<String, Object>> mdc = withInitial(HashMap::new);
+	private final ThreadLocal<Deque<JbossNdcEntry>> ndc = withInitial(ArrayDeque::new);
 
 	@Override
 	public org.jboss.logging.Logger getLogger(String name) {
@@ -21,64 +23,67 @@ public final class JbossProvider implements org.jboss.logging.LoggerProvider {
 
 	@Override
 	public void clearMdc() {
-		this.mdc.clear();
+		this.mdc.remove();
 	}
 
 	@Override
 	public Object putMdc(String key, Object value) {
-		return this.mdc.put(key, value);
+		return this.mdc.get().put(key, value);
 	}
 
 	@Override
 	public Object getMdc(String key) {
-		return this.mdc.get(key);
+		return this.mdc.get().get(key);
 	}
 
 	@Override
 	public void removeMdc(String key) {
-		this.mdc.remove(key);
+		this.mdc.get().remove(key);
 	}
 
 	@Override
 	public Map<String, Object> getMdcMap() {
-		return this.mdc;
+		return this.mdc.get();
 	}
 
 	@Override
 	public void clearNdc() {
-		this.ndc.clear();
+		this.ndc.remove();
 	}
 
 	@Override
 	public String getNdc() {
-		if (this.ndc.isEmpty()) return null;
-		return this.ndc.peek().merged;
+		if (this.ndc.get().isEmpty()) return null;
+		return this.ndc.get().peek().merged;
 	}
 
 	@Override
 	public int getNdcDepth() {
-		return this.ndc.size();
+		return this.ndc.get().size();
 	}
 
 	@Override
 	public String popNdc() {
-		if (this.ndc.isEmpty()) return "";
-		return this.ndc.pop().current;
+		if (this.ndc.get().isEmpty()) return "";
+		return this.ndc.get().pop().current;
 	}
 
 	@Override
 	public String peekNdc() {
-		if (this.ndc.isEmpty()) return "";
-		return this.ndc.peek().current;
+		if (this.ndc.get().isEmpty()) return "";
+		return this.ndc.get().peek().current;
 	}
 
 	@Override
 	public void pushNdc(String message) {
-		this.ndc.push(this.ndc.isEmpty() ? new JbossNdcEntry(message) : new JbossNdcEntry(this.ndc.peek(), message));
+		JbossNdcEntry entry = this.ndc.get().isEmpty() ?
+			new JbossNdcEntry(message) :
+			new JbossNdcEntry(this.ndc.get().peek(), message);
+		this.ndc.get().push(entry);
 	}
 
 	@Override
 	public void setNdcMaxDepth(int ndcMaxDepth) {
-		while (this.ndc.size() > ndcMaxDepth) this.ndc.pop();
+		while (this.ndc.get().size() > ndcMaxDepth) this.ndc.get().pop();
 	}
 }
