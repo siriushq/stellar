81c1aaee4e400bd915b52b7f795e371896ebcc8e
Mechite <contact@mechite.com>
2026-01-20 17:35:34 +0000

    Delete `stellar.lifecycle.natives.*`
    Related-to: 13197becdcb56228df0409580f3469fe0f2f2532
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- stellar.lifecycle.natives/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-import sirius.stellar.lifecycle.natives.NativesProcessor;
-
-module sirius.stellar.lifecycle.natives {
-	requires static io.avaje.prism;
-	requires static java.compiler;
-	requires static io.jstach.jstache;
-
-	provides javax.annotation.processing.Processor with NativesProcessor;
-}
