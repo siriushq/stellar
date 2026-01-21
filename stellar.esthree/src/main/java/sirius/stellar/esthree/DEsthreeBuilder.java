08cef13b3f2847eb11d0c473018733a5d4f51dca
Mechite <contact@mechite.com>
2026-01-21 00:40:48 +0000

    Simplify logic in `DEsthreeBuilder`
    - Use imperative style for null assertions instead of ternary operator
    - Do not use `@Nullable` access & secret key fields, use empty strings
    
    - Move building of `DocumentBuilderFactory` & `TransformerFactory`
      into individual methods, and make them, `#documentBuilder` and
      `#transformer` as `static` methods (a preemptive optimization)
    
    - Improve exception messages
    
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- stellar.esthree/src/main/java/sirius/stellar/esthree/DEsthreeBuilder.java
+++ stellar.esthree/src/main/java/sirius/stellar/esthree/DEsthreeBuilder.java
@@ -1,7 +1,6 @@
 package sirius.stellar.esthree;
 
 import io.avaje.http.client.HttpClient;
-import org.jspecify.annotations.Nullable;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -9,6 +8,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
+import java.util.StringJoiner;
 
 import static java.lang.System.getProperty;
 import static java.lang.System.getenv;
@@ -24,28 +24,40 @@ final class DEsthreeBuilder implements Esthree.Builder {
 	private final HttpClient.Builder httpClientBuilder;
 
 	private String region;
+
 	private String endpoint;
 	private boolean endpointOverride;
 	private boolean endpointVirtual;
 
-	private @Nullable String accessKey;
-	private @Nullable String secretKey;
+	private String accessKey;
+	private String secretKey;
 
 	DEsthreeBuilder() {
 		this.httpClientBuilder = HttpClient.builder();
 
-		String region = getProperty("aws.region", getenv().get("AWS_REGION"));
-		String accessKey = getProperty("aws.accessKeyId", getenv().get("AWS_ACCESS_KEY_ID"));
-		String secretKey = getProperty("aws.secretAccessKey", getenv().get("AWS_SECRET_ACCESS_KEY"));
-		String endpoint = getProperty("aws.endpointUrl", getenv().get("AWS_ENDPOINT_URL"));
+		this.region = getProperty("aws.region");
+		if (this.region == null) this.region = getenv().get("AWS_REGION");
+		if (this.region == null) this.region = US_EAST_1.toString();
 
-		this.region = (region != null) ? region : US_EAST_1.toString();
-		this.endpoint = (endpoint != null) ? endpoint : ("https://s3." + this.region + ".amazonaws.com");
-		this.endpointOverride = (endpoint != null);
-		this.endpointVirtual = (endpoint == null);
+		this.accessKey = getProperty("aws.accessKeyId");
+		if (this.accessKey == null) this.accessKey = getenv().get("AWS_ACCESS_KEY_ID");
+		if (this.accessKey == null) this.accessKey = "";
 
-		this.accessKey = accessKey;
-		this.secretKey = secretKey;
+		this.secretKey = getProperty("aws.secretAccessKey");
+		if (this.secretKey == null) this.secretKey = getenv().get("AWS_SECRET_ACCESS_KEY");
+		if (this.secretKey == null) this.secretKey = "";
+
+		this.endpoint = getProperty("aws.endpointUrl");
+		if (this.endpoint == null) this.endpoint = getenv().get("AWS_ENDPOINT_URL");
+
+		this.endpointOverride = true;
+		this.endpointVirtual = false;
+
+		if (this.endpoint == null) {
+			this.endpoint = ("https://s3." + this.region + ".amazonaws.com");
+			this.endpointOverride = false;
+			this.endpointVirtual = true;
+		}
 	}
 
 	@Override
@@ -76,65 +88,80 @@ final class DEsthreeBuilder implements Esthree.Builder {
 		return this.httpClientBuilder;
 	}
 
+	/// Creates a hardened [DocumentBuilderFactory].
+	private static DocumentBuilderFactory documentBuilderFactory() {
+		try {
+			DocumentBuilderFactory result = DocumentBuilderFactory.newDefaultInstance();
+
+			result.setXIncludeAware(false);
+			result.setExpandEntityReferences(false);
+
+			result.setFeature(FEATURE_SECURE_PROCESSING, true);
+			result.setAttribute(ACCESS_EXTERNAL_DTD, "");
+			result.setAttribute(ACCESS_EXTERNAL_SCHEMA, "");
+
+			return result;
+		} catch (ParserConfigurationException exception) {
+			throw new IllegalStateException("Failed to configure javax.xml DocumentBuilderFactory", exception);
+		}
+	}
+
 	/// Creates a [DocumentBuilder] using the provided factory, for thread-local instantiation.
-	private DocumentBuilder documentBuilder(DocumentBuilderFactory factory) {
+	private static DocumentBuilder documentBuilder(DocumentBuilderFactory factory) {
 		try {
 			return factory.newDocumentBuilder();
 		} catch (ParserConfigurationException exception) {
-			throw new IllegalStateException("Failed to create thread-local javax.xml DocumentBuilder in Esthree", exception);
+			throw new IllegalStateException("Failed to create thread-local javax.xml DocumentBuilder", exception);
+		}
+	}
+
+	/// Creates a hardened [TransformerFactory].
+	private static TransformerFactory transformerFactory() {
+		try {
+			TransformerFactory result = TransformerFactory.newInstance();
+
+			result.setFeature(FEATURE_SECURE_PROCESSING, true);
+			result.setAttribute(ACCESS_EXTERNAL_DTD, "");
+			result.setAttribute(ACCESS_EXTERNAL_STYLESHEET, "");
+
+			return result;
+		} catch (TransformerConfigurationException exception) {
+			throw new IllegalStateException("Failed to configure javax.xml TransformerFactory", exception);
 		}
 	}
 
 	/// Creates a [Transformer] using the provided factory, for thread-local instantiation.
-	private Transformer transformer(TransformerFactory factory) {
+	private static Transformer transformer(TransformerFactory factory) {
 		try {
 			Transformer transformer = factory.newTransformer();
 			transformer.setOutputProperty(ENCODING, UTF_8.name());
 			return transformer;
 		} catch (TransformerConfigurationException exception) {
-			throw new IllegalStateException("Failed to create thread-local javax.xml Transformer in Esthree", exception);
+			throw new IllegalStateException("Failed to create thread-local javax.xml Transformer", exception);
 		}
 	}
 
 	@Override
 	public Esthree build() {
-		if (this.accessKey == null
-			|| this.accessKey.isEmpty()
-			|| this.secretKey == null
-			|| this.secretKey.isEmpty()) throw new IllegalStateException("No credentials provided to Esthree Builder");
+		if (this.accessKey.isEmpty() || this.secretKey.isEmpty()) {
+			throw new IllegalStateException(new StringJoiner(", ")
+					.add("No credentials provided")
+					.add("accessKey.length = " + this.accessKey.length())
+					.add("secretKey.length = " + this.secretKey.length())
+					.toString());
+		}
 
 		EsthreeSigner signer = EsthreeSigner.create(this.accessKey, this.secretKey, this.region);
-		this.httpClientBuilder.baseUrl(this.endpoint);
+		HttpClient client = this.httpClientBuilder
+				.baseUrl(this.endpoint)
+				.build();
 
-		try {
-			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newDefaultInstance();
-			documentFactory.setXIncludeAware(false);
-			documentFactory.setExpandEntityReferences(false);
+		DocumentBuilderFactory documentBuilderFactory = documentBuilderFactory();
+		TransformerFactory transformerFactory = transformerFactory();
 
-			documentFactory.setFeature(FEATURE_SECURE_PROCESSING, true);
-			documentFactory.setAttribute(ACCESS_EXTERNAL_DTD, "");
-			documentFactory.setAttribute(ACCESS_EXTERNAL_SCHEMA, "");
+		ThreadLocal<DocumentBuilder> parser = withInitial(() -> documentBuilder(documentBuilderFactory));
+		ThreadLocal<Transformer> transformer = withInitial(() -> transformer(transformerFactory));
 
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			transformerFactory.setFeature(FEATURE_SECURE_PROCESSING, true);
-			transformerFactory.setAttribute(ACCESS_EXTERNAL_DTD, "");
-			transformerFactory.setAttribute(ACCESS_EXTERNAL_STYLESHEET, "");
-
-			ThreadLocal<DocumentBuilder> documentBuilder = withInitial(() -> documentBuilder(documentFactory));
-			ThreadLocal<Transformer> transformer = withInitial(() -> transformer(transformerFactory));
-
-			return new DEsthree(
-					signer,
-					this.httpClientBuilder.build(),
-					documentBuilder,
-					transformer,
-					this.region,
-					this.endpoint,
-					this.endpointVirtual);
-		} catch (ParserConfigurationException exception) {
-			throw new IllegalStateException("Failed to configure javax.xml DocumentBuilderFactory in Esthree Builder", exception);
-		} catch (TransformerConfigurationException exception) {
-			throw new IllegalStateException("Failed to configure javax.xml TransformerFactory in Esthree Builder", exception);
-		}
+		return new DEsthree(signer, client, parser, transformer, this.region, this.endpoint, this.endpointVirtual);
 	}
 }
