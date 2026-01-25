2b33084c79b656758339889d3bb1a1c17351cfc6
Mechite <contact@mechite.com>
2026-01-25 21:39:46 +0000

    Update `sirius.stellar.logging.package-info` (example)
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- stellar.logging/src/main/java/sirius/stellar/logging/package-info.java
+++ stellar.logging/src/main/java/sirius/stellar/logging/package-info.java
@@ -1,59 +1,68 @@
 /// Flexible logging system with a transparent interface and support for
 /// many facades and logging backends such as JCL, JUL, SLF4J, etc.
 ///
-/// For most users, the "best default" configuration would be to provide the
-/// [sirius.stellar.logging.collect.Collector#console] collector and a severity
-/// of [sirius.stellar.logging.LoggerLevel#ERROR]`.severity()`.
+/// For most users, the "best default" configuration would be to add a
+/// dependency on the console collector module, and potentially set a
+/// severity of [sirius.stellar.logging.LoggerLevel#ERROR]`.severity()`.
 ///
 /// The below example displays common usage patterns:
-/// ```
+/// {@snippet lang="java" :
 /// import sirius.stellar.logging.Logger;
-/// // import static sirius.stellar.logging.Logger.*;
+/// // or import static sirius.stellar.logging.Logger.*;
+///
 /// import static sirius.stellar.logging.LoggerLevel.*;
 ///
-/// public class Main {
+/// void main()
+/// {
+/// 	Logger.severity(ERROR.severity());
 ///
-///     public static void main(String[] arguments) {
-///         Logger.collector(Collector.console());
-///         Logger.severity(ERROR.severity());
+/// 	// Use the statically accessible methods for logging.
+/// 	Logger.information("Hello, world!");
 ///
-///         // use the statically accessible methods for logging
-///         Logger.information("Hello, world!");
+/// 	// Lazy-evaluate throwables. Due to ERROR severity being set above,
+/// 	// both the throwable and message string are never computed.
+/// 	Logger.stacktrace(
+/// 		() -> new Throwable("..."),
+/// 		() -> "Failed to reticulate splines"
+/// 	);
 ///
-///         // lazy-evaluate throwable
-///         // due to ERROR severity, this throwable is never computed
-///         Logger.stacktrace(() -> new Throwable("..."), "Failed to reticulate splines");
+/// 	// Add dependencies on dispatchers to use other APIs, as follows:
 ///
-///         // add dependencies on any dispatchers to use other API, as follows:
+/// 	var slf4j = org.slf4j.LoggerFactory
+/// 		.getLogger(Main.class);
+/// 	slf4j.info("Hello from SLF4j!");
 ///
-///         var slf4j = org.slf4j.LoggerFactory.getLogger(Main.class);
-///         slf4j.info("Hello from SLF4j!");
+/// 	var log4j = org.apache.log4j.Logger
+/// 		.getLogger(Main.class);
+/// 	log4j.info("Hello from Log4J!");
 ///
-///         var log4j = org.apache.log4j.Logger.getLogger(Main.class);
-///         log4j.info("Hello from Log4J!");
+/// 	var log4j2 = org.apache.logging.log4j.LogManager
+/// 		.getLogger(Main.class);
+/// 	log4j2.info("Hello from Log4J2!");
 ///
-///         var log4j2 = org.apache.logging.log4j.LogManager.getLogger(Main.class);
-///         log4j2.info("Hello from Log4J2!");
+/// 	var jul = java.util.logging.Logger
+/// 		.getLogger("org.example.Main");
+/// 	jul.info("Hello from java.util.logging!");
 ///
-///         var jul = java.util.logging.Logger.getLogger("org.example.Main");
-///         jul.info("Hello from java.util.logging!");
+/// 	var jcl = org.apache.commons.logging.LogFactory
+/// 		.getLog(Main.class);
+/// 	jcl.info("Hello from Jakarta/Apache Commons Logging!");
 ///
-///         var jcl = org.apache.commons.logging.LogFactory.getLog(Main.class);
-///         jcl.info("Hello from Jakarta/Apache Commons Logging!");
+/// 	var jboss = org.jboss.logging.Logger
+/// 		.getLogger(Main.class);
+/// 	jboss.info("Hello from JBoss!");
 ///
-///         var jboss = org.jboss.logging.Logger.getLogger(Main.class);
-///         jboss.info("Hello from JBoss!");
+/// 	var jsr379 = java.lang.System
+/// 		.getLogger("org.example.Main");
+/// 	var jsr379_INFO = java.lang.System.
+/// 		Logger.Level.INFO;
+/// 	jsr379.log(jsr379_INFO, "Hello from System.Logger!");
 ///
-///         var jsr379 = System.getLogger("org.example.Main");
-///         jsr379.log(System.Logger.Level.INFO, "Hello from System.Logger!");
+/// 	org.tinylog.Logger
+/// 		.info("Hello from tinylog!");
 ///
-///         org.tinylog.Logger.info("Hello from tinylog!");
-///
-///         com.esotericsoftware.minlog.Log.info("Hello from minlog!");
-///
-///         // add any collectors to your classpath, to have logs sent there
-///         // e.g., add SLF4J collector to use SLF4J-based logging backend, like Logback.
-///     }
+/// 	com.esotericsoftware.minlog.Log
+/// 		.info("Hello from minlog!");
 /// }
-/// ```
+/// }
 package sirius.stellar.logging;
