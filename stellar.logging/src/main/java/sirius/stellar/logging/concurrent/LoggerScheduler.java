aa70fce4c773c26e403e2b0358401bec03297fff
Mechite <contact@mechite.com>
2026-01-25 21:25:49 +0000

    Implement `LoggerScheduler#close` for Java <19
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- stellar.logging/src/main/java/sirius/stellar/logging/concurrent/LoggerScheduler.java
+++ stellar.logging/src/main/java/sirius/stellar/logging/concurrent/LoggerScheduler.java
@@ -6,6 +6,9 @@ import sirius.stellar.logging.Logger;
 import java.util.ServiceLoader;
 import java.util.concurrent.ScheduledExecutorService;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.stream.Collectors.joining;
+
 /// [ScheduledExecutorService] used by [Logger] for scheduling logging.
 /// This SPI allows for another implementation to be provided, if desired.
 ///
@@ -13,7 +16,8 @@ import java.util.concurrent.ScheduledExecutorService;
 /// a single virtual thread executor.
 ///
 /// @since 1.0
-public interface LoggerScheduler extends ScheduledExecutorService {
+public interface LoggerScheduler
+	extends ScheduledExecutorService, AutoCloseable {
 
 	/// Obtain a [LoggerScheduler] instance, service-loading the first
 	/// alternative implementation found on the class-path/module-path,
@@ -29,6 +33,29 @@ public interface LoggerScheduler extends ScheduledExecutorService {
 			return new DefaultLoggerScheduler();
 		} catch (Throwable throwable) {
 			throw new IllegalStateException("Failed wiring alternate logger scheduler", throwable);
+		}
+	}
+
+	/// Initiates an orderly shutdown where previously submitted tasks are
+	/// executed, but no new tasks will be accepted, waiting forever for all
+	/// tasks to complete their execution.
+	///
+	/// @throws IllegalStateException interrupted while waiting
+	/// @throws IllegalStateException failed termination
+	@Override
+	default void close() {
+		try {
+			boolean terminated = this.isTerminated();
+			if (!terminated) this.shutdown();
+
+			terminated = this.awaitTermination(Long.MAX_VALUE, NANOSECONDS);
+			if (!terminated) throw new IllegalStateException("Failed termination");
+		} catch (InterruptedException exception) {
+			String unexecuted = this.shutdownNow()
+					.stream()
+					.map(Runnable::toString)
+					.collect(joining(", ", "[", "]"));
+			throw new IllegalStateException("Interrupted " + unexecuted, exception);
 		}
 	}
 }
