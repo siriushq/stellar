siriushq/stellar

commit 9a48ca8230ff85d30ba13bc2f09eb89dd03b29a2

Add `stellar.security.ksuid`
Tested-by: Mahied Maruf <contact@mechite.com>
Signed-off-by: Mahied Maruf <contact@mechite.com>

stellar.security.ksuid/src/main/java/sirius/stellar/security/ksuid/Ksuid.java
@@ -1,0 +1,109 @@
+package sirius.stellar.security.ksuid;
+
+import sirius.stellar.serialization.base62.Base62;
+
+import java.security.SecureRandom;
+import java.time.Clock;
+import java.util.Random;
+import java.util.ServiceLoader;
+
+import static java.util.ServiceLoader.load;
+
+/// Service client for performing KSUID (K-Sortable Globally
+/// Unique Identifier) generation.
+///
+/// Generators are created using the static [#builder()] method.
+/// The default implementation of this client is fully thread-safe.
+///
+/// {@snippet lang = "java":
+/// Ksuid ksuid = Ksuid.builder()
+///         .random(new SecureRandom())
+///         .build();
+/// println(ksuid.identifier());
+/// }
+public interface Ksuid {
+
+	/// Generates a new random identifier. All implementations must
+	/// delegate [#toString] to the [Identifier#string] method.
+	Identifier identifier();
+
+	/// Parses the provided string input as an identifier.
+	/// @throws IllegalArgumentException short input provided
+	Identifier identifier(String input);
+
+	/// Parses the provided character array input as an identifier.
+	/// @throws IllegalArgumentException short input provided
+	Identifier identifier(char[] input);
+
+	/// Return a builder to construct [Ksuid] generator instances with.
+	static Builder builder() {
+		try {
+			ServiceLoader<Builder> loader = load(Builder.class);
+			for (Builder builder : loader) {
+				if (builder instanceof DKsuidBuilder) continue;
+				return builder;
+			}
+			return new DKsuidBuilder();
+		} catch (Throwable throwable) {
+			throw new IllegalStateException("Failed wiring alternate KSUID implementation", throwable);
+		}
+	}
+
+	/// @see Ksuid
+	interface Identifier extends CharSequence {
+
+		/// Returns this identifier formatted as a [Base62] unique string.
+		String string();
+
+		/// Returns this identifier formatted as a [Base62] character array.
+		char[] value();
+
+		@Override
+		default int length() {
+			return this.string().length();
+		}
+
+		@Override
+		default char charAt(int index) {
+			return this.string().charAt(index);
+		}
+
+		@Override
+		default CharSequence subSequence(int start, int end) {
+			return this.string().subSequence(start, end);
+		}
+	}
+
+	/// @see Ksuid
+	interface Builder {
+
+		/// Supply a [Random] generator instance.
+		/// The default is derived from [SecureRandom].
+		///
+		/// This method is a delegate of [#random(KsuidRandom)] using
+		/// [KsuidRandom#of(Random)] to use the provided instance.
+		default Builder random(Random random) {
+			return this.random(KsuidRandom.of(random));
+		}
+
+		/// Supply a [KsuidRandom] generator instance.
+		/// @see #random(Random)
+		Builder random(KsuidRandom random);
+
+		/// Supply a different [Clock] instance.
+		/// The default is derived from [Clock#systemDefaultZone].
+		///
+		/// This method is a delegate of [#clock(KsuidClock)] using
+		/// [KsuidClock#of(Clock)] to use the provided instance.
+		default Builder clock(Clock clock) {
+			return this.clock(KsuidClock.of(clock));
+		}
+
+		/// Supply a [KsuidClock] instance.
+		/// @see #clock(Clock)
+		Builder clock(KsuidClock clock);
+
+		/// Build and return the client (which is an [AutoCloseable]).
+		Ksuid build();
+	}
+}

pom.xml
@@ -54,6 +54,7 @@
 		<module>stellar.annotation.intellij</module>
 
 		<module>stellar.security.totp</module>
+		<module>stellar.security.ksuid</module>
 
 		<module>stellar.logging</module>
 
@@ -196,6 +197,11 @@
 			<dependency>
 				<groupId>org.siriushq.stellar</groupId>
 				<artifactId>stellar-security-totp</artifactId>
+				<version>1.0-RC1</version>
+			</dependency>
+			<dependency>
+				<groupId>org.siriushq.stellar</groupId>
+				<artifactId>stellar-security-ksuid</artifactId>
 				<version>1.0-RC1</version>
 			</dependency>
 

stellar.security.ksuid/pom.xml
@@ -1,0 +1,60 @@
+<project>
+	<modelVersion>4.0.0</modelVersion>
+
+	<parent>
+		<groupId>org.siriushq.stellar</groupId>
+		<artifactId>stellar</artifactId>
+		<version>1.0-RC1</version>
+		<relativePath>../pom.xml</relativePath>
+	</parent>
+
+	<packaging>jar</packaging>
+	<artifactId>stellar-security-ksuid</artifactId>
+	<name>Stellar :: Security :: KSUID</name>
+
+	<properties>
+		<java.version.main>11</java.version.main>
+		<java.version.test>11</java.version.test>
+	</properties>
+
+	<dependencies>
+		<dependency>
+			<groupId>org.siriushq.stellar</groupId>
+			<artifactId>stellar-serialization-base62</artifactId>
+		</dependency>
+		<dependency>
+			<groupId>org.siriushq.stellar</groupId>
+			<artifactId>stellar-lifecycle-testing</artifactId>
+			<scope>test</scope>
+		</dependency>
+	</dependencies>
+
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>org.apache.maven.plugins</groupId>
+				<artifactId>maven-jar-plugin</artifactId>
+			</plugin>
+			<plugin>
+				<groupId>org.apache.maven.plugins</groupId>
+				<artifactId>maven-compiler-plugin</artifactId>
+			</plugin>
+			<plugin>
+				<groupId>org.apache.maven.plugins</groupId>
+				<artifactId>maven-source-plugin</artifactId>
+			</plugin>
+			<plugin>
+				<groupId>org.apache.maven.plugins</groupId>
+				<artifactId>maven-javadoc-plugin</artifactId>
+			</plugin>
+			<plugin>
+				<groupId>org.apache.maven.plugins</groupId>
+				<artifactId>maven-surefire-plugin</artifactId>
+			</plugin>
+			<plugin>
+				<groupId>org.apache.maven.plugins</groupId>
+				<artifactId>maven-gpg-plugin</artifactId>
+			</plugin>
+		</plugins>
+	</build>
+</project>

stellar.security.ksuid/src/main/java/module-info.java
@@ -1,0 +1,11 @@
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+module sirius.stellar.security.ksuid {
+	requires org.jspecify;
+	requires sirius.stellar.serialization.base62x;
+
+	exports sirius.stellar.security.ksuid;
+
+	uses sirius.stellar.security.ksuid.Ksuid.Builder;
+}

stellar.security.ksuid/src/main/java/sirius/stellar/security/ksuid/DKsuid.java
@@ -1,0 +1,46 @@
+package sirius.stellar.security.ksuid;
+
+import sirius.stellar.serialization.base62.Base62;
+
+/// Domain implementation of [Ksuid].
+final class DKsuid implements Ksuid {
+
+	/// Constant offset from epoch time to allow extensibility in the
+	/// future by reducing the time from the standard 01/01/1970 epoch.
+	private static final long OFFSET = 1400000000L;
+
+	private final KsuidRandom random;
+	private final KsuidClock clock;
+
+	DKsuid(KsuidRandom random, KsuidClock clock) {
+		this.random = random;
+		this.clock = clock;
+	}
+
+	@Override
+	public Identifier identifier() {
+		byte[] bytes = new byte[20];
+		this.random.next(bytes);
+
+		long epoch = (this.clock.epoch() / 1000L) - OFFSET;
+		bytes[0] = (byte) (epoch >> 24);
+		bytes[1] = (byte) (epoch >> 16);
+		bytes[2] = (byte) (epoch >> 8);
+		bytes[3] = (byte) (epoch);
+
+		char[] encoded = Base62.encode(bytes);
+		return new DKsuidIdentifier(encoded);
+	}
+
+	@Override
+	public Identifier identifier(String input) {
+		if (input.length() < 27) throw new IllegalArgumentException("Short input provided");
+		return new DKsuidIdentifier(input.toCharArray());
+	}
+
+	@Override
+	public Identifier identifier(char[] input) {
+		if (input.length < 27) throw new IllegalArgumentException("Short input provided");
+		return new DKsuidIdentifier(input);
+	}
+}

stellar.security.ksuid/src/main/java/sirius/stellar/security/ksuid/DKsuidBuilder.java
@@ -1,0 +1,33 @@
+package sirius.stellar.security.ksuid;
+
+import org.jspecify.annotations.Nullable;
+
+/// Domain implementation of [Ksuid.Builder].
+final class DKsuidBuilder implements Ksuid.Builder {
+
+	@Nullable
+	private KsuidRandom random;
+
+	@Nullable
+	private KsuidClock clock;
+
+	@Override
+	public Ksuid.Builder random(KsuidRandom random) {
+		this.random = random;
+		return this;
+	}
+
+	@Override
+	public Ksuid.Builder clock(KsuidClock clock) {
+		this.clock = clock;
+		return this;
+	}
+
+	@Override
+	public Ksuid build() {
+		if (this.random == null) this.random = KsuidRandom.secure();
+		if (this.clock == null) this.clock = KsuidClock.system();
+
+		return new DKsuid(this.random, this.clock);
+	}
+}

stellar.security.ksuid/src/main/java/sirius/stellar/security/ksuid/DKsuidIdentifier.java
@@ -1,0 +1,26 @@
+package sirius.stellar.security.ksuid;
+
+/// Domain implementation of [Ksuid.Identifier].
+final class DKsuidIdentifier implements Ksuid.Identifier {
+
+	private final char[] value;
+
+	DKsuidIdentifier(char[] value) {
+		this.value = value;
+	}
+
+	@Override
+	public String string() {
+		return new String(this.value);
+	}
+
+	@Override
+	public char[] value() {
+		return this.value;
+	}
+
+	@Override
+	public String toString() {
+		return this.string();
+	}
+}

stellar.security.ksuid/src/main/java/sirius/stellar/security/ksuid/KsuidClock.java
@@ -1,0 +1,27 @@
+package sirius.stellar.security.ksuid;
+
+import java.time.Clock;
+
+/// Represents a clock for [Ksuid] purposes.
+///
+/// This is used to provide extensibility without depending on the
+/// stateful [java.time.Clock] (which this is a reduced wrapper of).
+///
+/// @see Ksuid.Builder#clock
+@FunctionalInterface
+public interface KsuidClock {
+
+	/// Returns the time of this clock ("the current time") in epoch
+	/// milliseconds ("since the beginning of time").
+	long epoch();
+
+	/// Create an instance from the provided [Clock].
+	static KsuidClock of(Clock clock) {
+		return clock::millis;
+	}
+
+	/// Create an instance using the system clock.
+	static KsuidClock system() {
+		return of(Clock.systemDefaultZone());
+	}
+}

commit ee5c62b2a14357b05df4042cc4f84d026f90465b

Fix bad Javadoc reference
Fixes: 9a48ca8230ff85d30ba13bc2f09eb89dd03b29a2
Signed-off-by: Mahied Maruf <contact@mechite.com>

stellar.security.ksuid/src/main/java/sirius/stellar/security/ksuid/Ksuid.java
@@ -24,7 +24,7 @@ import static java.util.ServiceLoader.load;
 public interface Ksuid {
 
 	/// Generates a new random identifier. All implementations must
-	/// delegate [#toString] to the [Identifier#string] method.
+	/// delegate [Object#toString] to the [Identifier#string] method.
 	Identifier identifier();
 
 	/// Parses the provided string input as an identifier.